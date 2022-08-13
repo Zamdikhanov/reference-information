e7ee90a70096394cd65174cd44864c0f779c119c
Zamdikhanov <oxothuk.sky@mail.ru>
2022-08-13 20:47:50 +0400

    add axios

---
+++ src/api/api.ts
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+const instance = axios.create({
+    baseURL: `https://62f77416ab9f1f8e89fd89a7.mockapi.io`,
+});
+
+const entityApi = {
+    getEntity({ entity='lamps', page = 0, limit = 0, options = '' }) {
+        return instance
+            .get(`/${entity}?${options}${page ? `page=${page}` : ''}${limit ?`&limit=${limit}`:''}`)
+            .catch(function (error) {
+                console.log('ошибка загрузки')
+            });
+    },
+    putEntity({ entity='lamps', id='0', data={} }) {
+        return instance.put(`/${entity}/${id}`, data);
+    },
+    postEntity({ entity='lamps', data={} }) {
+        return instance.post(`/${entity}`, data);
+    },
+    deleteEntity({ entity='lamps', id='0' }) {
+        return instance.delete(`/${entity}/${id}`);
+    },
+};
+
+export default entityApi;
--- src/pages/LampsPage/LampsPage.tsx
+++ src/pages/LampsPage/LampsPage.tsx
@@ -1,16 +1,27 @@
-import React from "react";
-import { lamps } from "./constants";
+import React, { useEffect, useState } from "react";
+import entityApi from "../../api/api";
 import Filters from "../../components/Filters/Filters";
 import LampCard from "../../components/LampCard/LampCard";
 import ProductLayout from "../../components/ProductLayout/ProductLayout";
+import { TLamp } from "./constants";
 
 function LampsPage() {
+
+let [lamps, setLamps]=useState([]);
+
+useEffect(()=>{
+  (async function() {
+    let lampsdd = await entityApi.getEntity({entity:'lamps'});
+    setLamps(lampsdd?.data);
+  })()
+},[])
+
   return (
       <ProductLayout
         title="Светильники"
         count={lamps?.length || 0}
         mainContent={
-          lamps.map((lamp) => { 
+          lamps.map((lamp: TLamp) => { 
             return <LampCard key={lamp.id} {...lamp} />;
           })
         }
